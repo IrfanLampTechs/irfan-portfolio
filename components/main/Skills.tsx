696e63059ea5dcb59999436380f369fb317770de
IrfanLampTechs <irfanazad588500@gmail.com>
2025-05-29 11:45:29 +0600

    6th commit

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,10 +1,18 @@
+import { scale } from 'framer-motion'
 import React from 'react'
 
 const Skills = () => {
   return (
-    <div>
-      
-    </div>
+    <section
+    id='skills'
+    className='flex flex-col items-center gap-3 h-full relative overflow-hidden py-20'
+    style={{transform: "scale(0.9)"}}
+    >
+      <div className=' flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+
+      </div>
+
+    </section>
   )
 }
 
