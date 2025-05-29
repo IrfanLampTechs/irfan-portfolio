df682a1f6aab6797b715532d70baff17b8798ceb
IrfanLampTechs <irfan@lamptechs.com>
2025-05-29 20:51:29 +0600

    7th commit

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,19 +1,28 @@
-import { scale } from 'framer-motion'
-import React from 'react'
+import { Frontend_skill } from "@/constants";
+import React from "react";
+import SkillDataProvider from "../sub/skillDataProvider";
+
 
 const Skills = () => {
   return (
     <section
-    id='skills'
-    className='flex flex-col items-center gap-3 h-full relative overflow-hidden py-20'
-    style={{transform: "scale(0.9)"}}
+      id="skills"
+      className="flex flex-col items-center gap-3 h-full relative overflow-hidden py-20"
+      style={{ transform: "scale(0.9)" }}
     >
-      <div className=' flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
-
+      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Frontend_skill.map((image, index) => (
+          <SkillDataProvider
+            key={index}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
